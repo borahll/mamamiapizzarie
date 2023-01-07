e4fd49bb58e7111cdc452f7c29f98ee6b02cadfc
borahll <bora.haliloglu@ug.bilkent.edu.tr>
2023-01-07 09:34:36 +0300

    s

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,8 +1,8 @@
 import sys
 
 
-def isvalid(matrixlen, x, y):  # check if the provided x and y are valid in the matrix
-    if 0 <= x < matrixlen and 0 <= y < matrixlen:
+def isvalid(matrixlen, x1, y):  # check if the provided x and y are valid in the matrix
+    if 0 <= x1 < matrixlen and 0 <= y < matrixlen:
         return True
     else:
         return False
@@ -11,41 +11,39 @@ def isvalid(matrixlen, x, y):  # check if the provided x and y are valid in the 
 def findmax(amatrix):  # function to return the maximum number in a matrix
     max1 = amatrix[0][0]
     for row in amatrix:
-        for column in amatrix:
-            if max1 < amatrix[row][column]:
-                max1 = amatrix[row][column]
+        for element in row:
+            if max1 < element:
+                max1 = element
     return max1
+
+
+def printmatrix(amatrix):
+    for row in amatrix:
+        print(" ".join(str(element) for element in row))
 
 
 first_line = sys.stdin.readline()
 x = first_line.split(' ')
 matrix_lenght = x[0]
 num_of_pizzarias = x[1]
-matrix = [[0] * int(matrix_lenght)] * int(matrix_lenght)
+matrix = [[0 for j in range(int(matrix_lenght))] for i in range(int(matrix_lenght))]
+printmatrix(matrix)
 matrix_lenght = int(matrix_lenght)
 origin_of_pizzeria_x = ""
 origin_of_pizzeria_y = ""
 serve_radius = ""
 
-sys.stdin.readline()
+
 for i in range(int(num_of_pizzarias)):
     line = sys.stdin.readline()
-    for ch in line:
-        if ch == " ":
-            break
-        origin_of_pizzeria_x += ch
-    for ch in line:
-        if ch == " ":
-            break
-        origin_of_pizzeria_y += ch
-    for ch in line:
-        if ch == " ":
-            break
-        serve_radius += ch
+    x = line.split(" ")
+    origin_of_pizzeria_x = x[0]
+    origin_of_pizzeria_y = x[1]
+    serve_radius = x[2]
     origin_of_pizzeria_x = int(origin_of_pizzeria_x) - 1
     origin_of_pizzeria_y = int(origin_of_pizzeria_y) - 1
-    print(origin_of_pizzeria_y)
     serve_radius = int(serve_radius)
+    print(f"{origin_of_pizzeria_y} {origin_of_pizzeria_x} {serve_radius}")
     start_x = origin_of_pizzeria_x
     start_y = origin_of_pizzeria_y
     matrix[start_x][start_y] += 1
@@ -66,14 +64,15 @@ for i in range(int(num_of_pizzarias)):
         start_y = origin_of_pizzeria_y + tempserve
         if isvalid(matrix_lenght, start_x, start_y):
             for a in range(tempserve):
+                tempxx = tempx
+                tempyy = tempy
                 for b in range(counter):
-                    tempxx = tempx
-                    tempyy = tempy
                     if isvalid(matrix_lenght, tempxx, tempyy):
                         matrix[tempxx][tempyy] += 1
                         tempxx += 1
                         tempyy -= 1
                 tempy -= 1
+                printmatrix(matrix)
             first = True
         else:
             tempserve -= 1
@@ -85,14 +84,15 @@ for i in range(int(num_of_pizzarias)):
         start_y = origin_of_pizzeria_y + tempserve
         if isvalid(matrix_lenght, start_x, start_y):
             for a in range(tempserve):
+                tempxx = tempx
+                tempyy = tempy
                 for b in range(counter):
-                    tempxx = tempx
-                    tempyy = tempy
                     if isvalid(matrix_lenght, tempxx, tempyy):
                         matrix[tempxx][tempyy] += 1
                         tempxx -= 1
                         tempyy -= 1
                 tempy -= 1
+                #printmatrix(matrix)
             second = True
         else:
             tempserve -= 1
@@ -104,14 +104,15 @@ for i in range(int(num_of_pizzarias)):
         start_y = origin_of_pizzeria_y - (tempserve + 1)
         if isvalid(matrix_lenght, start_x, start_y):
             for a in range(tempserve):
+                tempxx = tempx
+                tempyy = tempy
                 for b in range(counter):
-                    tempxx = tempx
-                    tempyy = tempy
                     if isvalid(matrix_lenght, tempxx, tempyy):
                         matrix[tempxx][tempyy] += 1
                         tempxx -= 1
                         tempyy += 1
                 tempy += 1
+                #printmatrix(matrix)
             third = True
         else:
             tempserve += 1
@@ -123,21 +124,21 @@ for i in range(int(num_of_pizzarias)):
         start_y = origin_of_pizzeria_y - (tempserve + 1)
         if isvalid(matrix_lenght, start_x, start_y):
             for a in range(tempserve):
+                tempxx = tempx
+                tempyy = tempy
                 for b in range(counter):
-                    tempxx = tempx
-                    tempyy = tempy
                     if isvalid(matrix_lenght, tempxx, tempyy):
                         matrix[tempxx][tempyy] += 1
                         tempxx += 1
                         tempyy += 1
                 tempy += 1
+                #printmatrix(matrix)
             fourth = True
         else:
             tempserve += 1
     if first and second:
         temp = serve_radius
         for a in range(serve_radius):
-            print(serve_radius)
             matrix[origin_of_pizzeria_x][origin_of_pizzeria_y + temp] -= 1
             temp -= 1
     if third and fourth:
@@ -145,4 +146,4 @@ for i in range(int(num_of_pizzarias)):
         for a in range(serve_radius):
             matrix[origin_of_pizzeria_x][origin_of_pizzeria_y - (temp + 1)] -= 1
             temp -= 1
-print(f' The maximum is  {findmax(matrix)}')
+print(f" The maximum is  {findmax(matrix)}")
